08e9923add01bf34196944033f2c67cfb5812cf3
kr4ssimir@freemail.hu <kr4ssimir@freemail.hu>
2019-01-02 13:42:54 +0100

    Accept base64-encoded *.m3u8-link

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const URL = require('url')
 const PATH = require('path')
 const PORT = process.env.PORT || 5000
 express().get('/add.json', (req, res) => {
-  youtubedl.getInfo(decodeURIComponent(req.query.url.replace(/^http:\/\//i, 'https://')), [], function(err, info) {
+  let url = req.query.url
+  m3u8 = () => {
+    url = Buffer.from(url, 'base64').toString('utf8')
+    let jsonObj = {
+      title: decodeURIComponent(req.query.title),
+      live: req.query.live == "true",
+      sources: [
+        {
+          url: url,
+          quality: [240, 360, 480, 540, 720, 1080, 1440].includes(Number(req.query.quality)) ? Number(req.query.quality) : 720,
+          contentType: req.query.type && decodeURIComponent(req.query.type) || 'application/x-mpegURL',
+          duration: decodeURIComponent(req.query.duration)
+        }
+      ]
+    }
+    res.send(jsonObj)
+  }
+  req.query.url && req.query.m3u8 ? m3u8() : req.query.url && youtubedl.getInfo(decodeURIComponent(req.query.url.replace(/^http:\/\//i, 'https://')), [], function(err, info) {
     if (err) console.error(err)
     else {
       if (!info.title) info = info[0];
@@ -23,7 +40,6 @@ express().get('/add.json', (req, res) => {
         ].find(contentType => contentType.ext.includes(ext))
         if (contentType != undefined) return contentType.type
       }
-
       let url = info.manifest_url ? info.manifest_url.replace(/^http:\/\//i, 'https://') : info.url.replace(/^http:\/\//i, 'https://')
       let jsonObj = {
         title: req.query.title ? decodeURIComponent(req.query.title) : !info.title.toLowerCase().startsWith(info.extractor_key.toLowerCase()) ? info.extractor_key + ' - ' + info.title : info.title,
@@ -38,9 +54,9 @@ express().get('/add.json', (req, res) => {
       }
       if (![240, 360, 480, 540, 720, 1080, 1440].includes(jsonObj.sources[0].quality)) jsonObj.sources[0].quality = 720;
       if (info.thumbnail && info.thumbnail.startsWith('http')) jsonObj.sources[0].thumbnail = info.thumbnail.replace(/^http:\/\//i, 'https://')
-      if (req.query.duration) jsonObj.duration = Number(req.query.duration)
+      if (decodeURIComponent(req.query.duration)) jsonObj.duration = Number(req.query.duration)
       else if (info._duration_raw) jsonObj.duration = info._duration_raw
-      if (!jsonObj.duration && !jsonObj.live) getVideoDurationInSeconds(url).then((duration) => {
+      if (!jsonObj.live && !jsonObj.duration) getVideoDurationInSeconds(url).then((duration) => {
         jsonObj.duration = duration
         res.send(jsonObj)
       }).catch(() => {
