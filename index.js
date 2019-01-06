88805abf95f3a7d6d0560f51b4dac69cad9851bd
kr4ssimir@freemail.hu <kr4ssimir@freemail.hu>
2019-01-06 01:14:08 +0100

    New site

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ express().get('/add.json', (req, res) => {
     const jsonObj = {
       title: decodeURIComponent(req.query.title),
       live: req.query.live == "true",
-      duration: Number(req.query.duration),
+      duration: Number(req.query.duration) || 0,
       sources: [
         {
           url: decodeURIComponent(req.query.url).replace(/^http:\/\//i, 'https://'),
@@ -23,20 +23,41 @@ express().get('/add.json', (req, res) => {
     if (jsonObj.sources[0].url.match(/.*\.m3u8/)) {
       tryToGetDurationAndSend(jsonObj)
     }
-    else if (jsonObj.sources[0].url.match(/https?:\/\/(www.)?nxload.com\/(embed-)?\w+.html/i)) {
+    else if (jsonObj.sources[0].url.match(/https?:\/\/(www\.)?nxload\.com\/(embed-)?\w+\.html/i)) {
       request(jsonObj.sources[0].url.replace(/embed-/i, ''), (err, response, body) => {
         if (err) return console.log(err)
         if (response.statusCode == 200) {
           const regMatch = body.match(/new Clappr\.Player\({\s+sources: \["([^"]+)/i)
           if (regMatch) {
-            jsonObj.sources[0].url = regMatch[1]
+            jsonObj.sources[0].url = regMatch[1].replace(/^http:\/\//i, 'https://')
             jsonObj.title = body.match(/<title>Watch ([^<]+)/i)[1],
             tryToGetDurationAndSend(jsonObj)
           }
         }
       })
     }
-    else youtubedl.getInfo(decodeURIComponent(jsonObj.sources[0].url), [], function(err, info) {
+    else if (jsonObj.sources[0].url.match(/https?:\/\/(www\.)?kinoger\.to\/stream\/[-\w]+\.html/i)) {
+      request(jsonObj.sources[0].url, (err, response, body) => {
+        if (err) return console.log(err)
+        if (response.statusCode == 200) {
+          let regMatch = body.match(/<div id="kinog-player"><iframe src="https?:\/\/([^"]+)/i)
+          if (regMatch) {
+            jsonObj.title = body.match(/<meta property="og:title" content="([^""]+)/i)[1],
+            request('https://s1.' + regMatch[1], (err, response, body) => {
+              if (err) return console.log(err)
+              if (response.statusCode == 200) {
+                regMatch = body.match(/', type: 'video\/mp4'},{url: \'\/\/([^\']+)/i)
+                if (regMatch) {
+                  jsonObj.sources[0].url = 'https://' + regMatch[1]
+                  tryToGetDurationAndSend(jsonObj)
+                }
+              }
+            })
+          }
+        }
+      })
+    }
+    else youtubedl.getInfo(jsonObj.sources[0].url, [], function(err, info) {
       if (err) console.error(err)
       else {
         if (!info.title) info = info[0];
