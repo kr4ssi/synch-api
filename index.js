51127e39e9155d0c5643f496897fc30d3d77677a
kr4ssimir@freemail.hu <kr4ssimir@freemail.hu>
2019-02-03 17:00:06 +0100

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ express().get('/add.json', (req, res) => {
       const md5ip = crypto.createHash('md5').update(ip).digest('hex')
       const userprovided = STATICS.find(obj => obj.url === originalUrl && obj.ip === md5ip)
       if (typeof userprovided != 'undefined') res.redirect(redirto)
-      else res.redirect(req.query.redirto)
+      else res.redirect(redirto)
     }
     else {
       const hourago = Date.now() - (60 * 60 * 1000)
