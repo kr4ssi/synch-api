2191ffc3f6bcc5b226af40db703c2d21c62f6612
kr4ssi@tuta.io <kr4ssi@tuta.io>
2019-03-18 04:24:17 +0100

    commit

--- linkupdate.sh
+++ linkupdate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl "https://synch-api.herokuapp.com/add.json?url=$1&userlink=$(youtube-dl -f best -g "$1")"
+curl "https://synchapi.herokuapp.com/add.json?url=$1&userlink=$(youtube-dl -f best -g "$1")"
